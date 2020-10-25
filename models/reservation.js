817d484fa4b99455e7dea88bd61623b5dcebf549
unknown <jreidmke@gmail.com>
2020-10-25 11:58:46 -0500

    fixed save reservation bug

--- models/reservation.js
+++ models/reservation.js
@@ -38,22 +38,22 @@ class Reservation {
 
     return results.rows.map(row => new Reservation(row));
   }
-
   async save() {
     if(this.id === undefined) {
       const result = await db.query(`
       INSERT INTO reservations (customer_id, start_at, num_guests, notes)
-      VALUES ($1, $2, $3, 44)
+      VALUES ($1, $2, $3, $4)
       RETURNING id
       `, [this.customerId, this.startAt, this.numGuests, this.notes]);
       this.id = result.rows[0];
     } else {
       await db.query(`
-        UPDATE reservations SET customer_id=$1, start_at=$2, num_guests=$3, notes=$4
-        WHERE id=$5
-      `, [this.customerId, this.startAt, this.numGuests,this.notes])
+        UPDATE reservations SET start_at=$1, num_guests=$2, notes=$3
+        WHERE id=$4
+      `, [this.startAt, this.numGuests, this.notes, this.id])
     }
   }
+
 }
 
 
